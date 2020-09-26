a853d7aa39e4cf2e7849b81399203047bff7bfca
Your Name <gourav.saini.2703@gmail.com>
2020-09-26 15:15:58 +0200

    telegram notification added

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const https = require("https");
 const Telegraf = require('telegraf');
 const url = "https://api.mensa.legacymo.de/0";
 const config = require('./config.json');
-const app = new Telegraf(config.bot_token);
+const bot = new Telegraf(config.bot_token);
+const emoji = require('node-emoji');
+const noIdea = emoji.get('woman-shrugging');
+var message = "";
+var checkTime = "";
+
+setInterval(function(){ // Set interval for checking
+    checkTime = new Date(); // Date object to find out what time it is
+      if(checkTime.getHours() === 15 /*&& date.getMinutes() === 0*/){ // Check the time
+             // Code
 
 https.get(url, res => {
   res.setEncoding("utf8");
@@ -15,35 +24,55 @@ https.get(url, res => {
     var count = Object.keys(body.food).length;
     var i = 0;
     var isEmpty = 0;
-    var result = [];
     
 for (i = 0; i < count ; i++) {
     isEmpty = Object.keys(body.food[i].meal).length;   
     if( isEmpty  !== 0){
-    var veganCheck = JSON.stringify(body.food[i].meal[0].name).toLowerCase().includes("vegan",0);   
-        if(veganCheck)
-        {
-    var type = JSON.stringify(body.food[i].type);
-    var mealName = JSON.stringify(body.food[i].meal[0].name);
-    var mealCost = JSON.stringify(body.food[i].meal[0].costs.a);
-    var array = {
-        type : type,
-        mealname : mealName,
-        cost : mealCost
+   
+    var date = body.date;      
+    var type = (body.food[i].type);
+    var mealName = (body.food[i].meal[0].name);
+    var mealCostS = (body.food[i].meal[0].costs.a);
+    var mealCostB = (body.food[i].meal[0].costs.b);
+    var cost;
+    var messageDate;
+    if (mealCostS === "" && mealCostB === "" ){
+     cost = noIdea
     }
-     result.push(array);
-        }
+    else {
+        cost = "Studierende- " + mealCostS+"\nBedienstete- "+mealCostB;
+    }
+    message += "\n\n"+type+ "\n" +mealName+ "\n" +"Preis - "+cost;
+   
     }
 }
-console.log(result);
-app.hears('supp?', ctx => {
-    return ctx.reply(result);
+
+if (date === new Date().getDate()){
+    messageDate = "Heutiges Angebot";
+}
+else {
+    messageDate = "Angebot am " + date;
+}
+
+
+var finalMessage = messageDate+message;
+console.log(finalMessage);
+
+//****Telegram bot start*****
+bot.hears('Supp?', ctx => {
+    return ctx.reply(finalMessage);
+    
   });
-  
-  app.startPolling();
-
-
-
-
-  });
+  bot.telegram.sendMessage('@UniBMensaMenu', finalMessage);
+  bot.startPolling();
+//****Telegram bot end*****
+message = "";
+ 
 });
+});
+
+
+}
+},3600010); // Repeat every 60000 milliseconds (1 minute)
+
+console.log(new Date().getHours());

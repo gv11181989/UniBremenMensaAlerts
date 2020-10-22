465f270b23d161d6ff05275dab4be6b4f2be8a0f
Your Name <gourav.saini.2703@gmail.com>
2020-10-22 05:10:52 +0200

    mealType Scraper script file added

---
+++ scraper.js
@@ -0,0 +1,39 @@
+// Scraper code start
+
+const cheerio = require('cheerio'),
+ axios = require('axios'),
+ scraperurl = 'https://www.stw-bremen.de/de/mensa/uni-mensa';
+var mealArray = [];
+var scraper = axios.get(scraperurl).then(function (response) {
+  const $ = cheerio.load(response.data);
+  //const count = $("table").last().index();
+  const table = $("table");
+
+
+  for (i = 0; i < 5; i++) {
+    //var areaType = table.eq(i).find("thead > tr >th").eq(0).text();//.replace(/[\s]/g, "");
+    var mealType = table.eq(i).find("tbody > tr > td > img").eq(0).attr("alt");
+    //var mealName = table.eq(i).find("tbody > tr > td").eq(1).text().replace(/[\s]/g, "");
+
+    if (typeof mealType == 'undefined') {
+      mealType = " ";
+    }
+    else {
+      mealType = mealType;
+    }
+    var meal = {
+     // "areaType": areaType,
+      "mealType": mealType,
+     // "mealName": mealName
+    };
+    mealArray.push(meal);
+  }
+  //console.log(mealArray);
+  return mealArray;     
+  });
+  exports.toServerjs = scraper;
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const https = require("https");
 const Telegraf = require('telegraf');
 const url = "https://api.mensa.legacymo.de/0";
@@ -9,135 +10,91 @@ const runEmoji = emoji.get('woman-running');
 const nerd = emoji.get('nerd_face');
 var message = "";
 var checkTime = "";
-var finalMessage ="";
+var finalMessage = "";
 console.log("App running");
+setInterval(() => {
+  try {
+    var fromScraperjs = require('./scraper.js');
+    fromScraperjs.toServerjs.then(function (result) {
+      //  console.log(result);
+      //});
 
-// Scraper code start
-setInterval(function(){ // Set interval for checking     
-const cheerio = require('cheerio'),
- axios = require('axios'), 
- scraperurl = 'https://www.stw-bremen.de/de/mensa/uni-mensa';
- var mealArray = []; 
-axios.get(scraperurl)
-    .then((response) => {
-// Load the HTML code as a string, which returns a Cheerio instance
-const $ = cheerio.load(response.data)
-const count = $("table").last().index();
-const table = $("table");
+      https.get(url, res => {
+        res.setEncoding("utf8");
+        let body = "";
+        res.on("data", data => {
+          body += data;
+        });
+        res.on("end", () => {
+          try {
+            body = JSON.parse(body);
+            var count = Object.keys(body.food).length;
+            var i = 0;
+            var isEmpty = 0;
+
+            for (i = 0; i < count; i++) {
+              isEmpty = Object.keys(body.food[i].meal).length;
+              if (isEmpty !== 0) {
+
+                var date = body.date;
+                var type = (body.food[i].type);
+                var mealType = result[i].mealType;
+                var mealName = (body.food[i].meal[0].name);
+                var mealCostS = (body.food[i].meal[0].costs.a);
+                var mealCostB = (body.food[i].meal[0].costs.b);
+                var cost;
+                var messageDate;
+                if (mealCostS === "" && mealCostB === "") {
+                  cost = noIdea
+                }
+                else {
+                  cost = "Studierende- " + mealCostS + "\nBedienstete- " + mealCostB;
+                }
+                message += "\n\n" + type + "\n" + mealType + "\n" + mealName + "\n" + "Preis - " + cost;
+
+              }
+            }
+
+            var dateNumber = date.replace(/\D/g, '');
+
+            if (Number(dateNumber) === new Date().getDate()) {
+              messageDate = "Heutiges Angebot";
+            }
+            else {
+              messageDate = "Angebot am " + date;
+            }
 
 
-for (i=0 ; i < count ; i++ ){
-  var areaType = table.eq(i).find("thead > tr >th").eq(0).text();//.replace(/[\s]/g, "");//.substr(16, 8);
-  var mealType = table.eq(i).find("tbody > tr > td > img").eq(0).attr("alt");
-  var mealName = table.eq(i).find("tbody > tr > td").eq(1).text().replace(/[\s]/g, "");//.substr(0, 10);;
-
-  if (typeof mealType == 'undefined'){
-    mealType = " ";
-  }
-  else {
-    mealType = mealType;
-  }
-  var meal = {
-    "areaType" : areaType,
-    "mealType" : mealType,
-    "mealName" : mealName
-  };
-   mealArray.push(meal);
-}
-console.log(mealArray);
+            finalMessage = messageDate + message;
+            console.log(finalMessage);
+          }
+          catch (e) {
+            console.log(e);
+            finalMessage = "Sorry, can't reach the menu today.The resident nerd" + nerd + "is looking into this issue" + runEmoji;
+          }
+        });
+      });
 
 
-
-  https.get(url, res => {  
-    res.setEncoding("utf8");
-    let body = "";
-    res.on("data", data => {
-      body += data;
     });
-    res.on("end", () => {
-      try {
-      body = JSON.parse(body);
-      var count = Object.keys(body.food).length;
-      var i = 0;
-      var isEmpty = 0;
-      
-  for (i = 0; i < count ; i++) {
-      isEmpty = Object.keys(body.food[i].meal).length;   
-      if( isEmpty  !== 0){
-     
-      var date = body.date;      
-      var type = (body.food[i].type);
-      var mealType = mealArray[i].mealType;
-      var mealName = (body.food[i].meal[0].name);
-      var mealCostS = (body.food[i].meal[0].costs.a);
-      var mealCostB = (body.food[i].meal[0].costs.b);
-      var cost;
-      var messageDate;
-      if (mealCostS === "" && mealCostB === "" ){
-       cost = noIdea
-      }
-      else {
-          cost = "Studierende- " + mealCostS+"\nBedienstete- "+mealCostB;
-      }
-      message += "\n\n"+type+ "\n" +mealType+ "\n" +mealName+ "\n" +"Preis - "+cost;
-     
-      }
-  }
-  
-  var dateNumber = date.replace(/\D/g, '');
-  
-  if (Number(dateNumber) === new Date().getDate()){
-      messageDate = "Heutiges Angebot";
-  }
-  else {
-      messageDate = "Angebot am " + date;
-  }
-  
-  
-   finalMessage = messageDate+message;
-  //console.log(finalMessage);
-      }
-      catch (e) {
-        console.log(e);
-        finalMessage = "Sorry, can't reach the menu today.The resident nerd" +nerd+ "is looking into this issue" +runEmoji;
-      }
-  
-   
-  });
-  
-  }).on("error", ()=> {
+  } catch (e) {
     console.log(e);
-    finalMessage = "Sorry, can't reach the menu today.The resident nerd " +nerd+ " is looking into this issue " +runEmoji;
-    
-  });
-  
+    finalMessage = "Sorry, can't reach the menu today.The resident nerd" + nerd + "is looking into this issue" + runEmoji;
+  }
   //****Telegram bot start*****
   bot.hears('?', ctx => {
     return ctx.reply(finalMessage);
-    
+
   });
-  
-    checkTime = new Date(); // Date object to find out what time it is
-      if(checkTime.getHours() === 5 && checkTime.getMinutes() === 0){ // Check the time
-  
-  bot.telegram.sendMessage('@UniBMensaMenu', finalMessage);
-  console.log(new Date().getHours());
+
+  checkTime = new Date(); // Date object to find out what time it is
+  if (checkTime.getHours() === 5 && checkTime.getMinutes() === 0) { // Check the time
+
+    bot.telegram.sendMessage('@UniBMensaMenu', finalMessage);
+    console.log(new Date().getHours());
   }
   bot.startPolling();
-  //****Telegram bot end*****
-  
+  //****Telegram bot end*****  
   message = "";
-
-}).catch(function (e) {
-  console.log(e);
-});
-
-},60000);
-//Scraper code finish
-
-
-
-
-
-
+}, 60000);
 

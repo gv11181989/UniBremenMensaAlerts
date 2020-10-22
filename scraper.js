92402d8f95b8fce0ac159aa86a159d7b882d351e
Your Name <gourav.saini.2703@gmail.com>
2020-10-22 19:59:07 +0200

    table index count

--- scraper.js
+++ scraper.js
@@ -6,11 +6,11 @@ const cheerio = require('cheerio'),
 var mealArray = [];
 var scraper = axios.get(scraperurl).then(function (response) {
   const $ = cheerio.load(response.data);
-  //const count = $("table").last().index();
+  const count = $("body").find('div').eq(13).find('.l-main').find('div > div > div > div').eq(43).find('table').last().index();
   const table = $("table");
 
 
-  for (i = 0; i < 5; i++) {
+  for (i = 0; i < count; i++) {
     //var areaType = table.eq(i).find("thead > tr >th").eq(0).text();//.replace(/[\s]/g, "");
     var mealType = table.eq(i).find("tbody > tr > td > img").eq(0).attr("alt");
     //var mealName = table.eq(i).find("tbody > tr > td").eq(1).text().replace(/[\s]/g, "");
